80e49b454e16cac572ecdcaae2ca1c88cc982ef4
George Kolesnikov <gkolesnikov88@gmail.com>
2022-10-06 06:55:20 +0300

    Extend PostBox form with react-hook-form

--- components/Postbox.tsx
+++ components/Postbox.tsx
@@ -1,22 +1,38 @@
+import { LinkIcon, PhotoIcon } from '@heroicons/react/24/outline';
 import { useSession } from 'next-auth/react'
 import React from 'react'
 import Avatar from './Avatar';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+    postTitle: string;
+    postBody: string;
+    postImage: string;
+    subreddit: string;
+};
 
 function Postbox() {
   const { data: session } = useSession();
+  const { register, handleSubmit, watch, formState: { errors } } =
+    useForm<FormData>();
 
   return (
-    <form>
+    <form className='sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2'>
         <div className='flex items-center space-x-3'>
             <Avatar/>
 
-            <input type="text"
+            <input 
+                {...register('postTitle', {required: true})}
+                type="text"
                 disabled={!session} 
                 className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
                 placeholder={ session ?
                     'Create a post by entering a title' :
                     'Sign in to post'
-                }/>
+            }/>
+
+            <PhotoIcon className={`h-6 text-gray-300 cursor-pointer`}/>
+            <LinkIcon className={`h-6 text-gray-300 cursor-pointer`}/>
         </div>
     </form>
   )
