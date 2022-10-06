221f9172f45e43b1734b50d363144848edc32384
George Kolesnikov <gkolesnikov88@gmail.com>
2022-10-06 06:22:11 +0300

    Add postbox component

---
+++ components/Postbox.tsx
@@ -0,0 +1,24 @@
+import { useSession } from 'next-auth/react'
+import React from 'react'
+
+function Postbox() {
+  const { data: session } = useSession();
+
+  return (
+    <form>
+        <div className='flex items-center space-x-3'>
+            {/* avatar */}
+
+            <input type="text"
+                disabled={!session} 
+                className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
+                placeholder={ session ?
+                    'Create a post by entering a title' :
+                    'Sign in to post'
+                }/>
+        </div>
+    </form>
+  )
+}
+
+export default Postbox;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Postbox from '../components/Postbox'
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +8,17 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit 2.0 Clone</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>     
+      </Head>
+
+      {/* PostBox */}
+      <Postbox/>
+
+      <div>
+
+        {/* Feed */}
+
+      </div>
+
     </div>
   )
 }
