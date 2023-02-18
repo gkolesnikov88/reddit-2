8be894214910ca33c6120cec60a0a22e4b9ceb82
George Kolesnikov <gkolesnikov88@gmail.com>
2023-02-18 11:36:36 +0300

    Added a Top Communities side section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import { useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import Postbox from '../components/Postbox'
+import SubredditRow from '../components/SubredditRow'
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries'
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+    variables: {
+      limit: 10
+    }
+  });
+
+  const subreddits: Subreddit[] = data?.getSubredditListLimit;
+
   return (
     <div className="my-7 mx-auto max-w-5xl">
       <Head>
@@ -11,10 +22,26 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Postbox/>
+      <Postbox />
 
       <div className='flex'>
-        <Feed/>
+        <Feed />
+
+        <div className='sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] 
+          rounded-md border border-gray-300 bg-white lg:inline'>
+          <p className='text-md mb-1 p-4 pb-3 font-bold'>Top Communities</p>
+
+          <div>
+            { subreddits?.map((subreddit,index) => (
+              <SubredditRow 
+                key={subreddit.id} 
+                topic={subreddit.topic} 
+                index={index}
+              />
+            ))
+            }
+          </div>
+        </div>
       </div>
 
     </div>
